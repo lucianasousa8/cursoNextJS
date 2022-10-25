a2d9d49df0751a611356ec7b898f6a36ab7588c0
LucianaLopes21 <lopesluciana320@gmail.com>
2022-10-25 19:15:02 -0300

    Exemplo aula 1

---
+++ string.js
@@ -0,0 +1,14 @@
+const cidade = "belo horizonte";
+const input = "Belo Horizonte";
+
+console.log(cidade === input); // false
+
+const cidade1 = "belo horizonte";
+const input2 = "Belo Horizonte";
+
+const inputMinusculo = input2.toLowerCase();
+
+console.log(cidade1 === inputMinusculo); // true
+
+const senha = "minhaSenha123"
+console.log(senha.length) // 13 caracteres
